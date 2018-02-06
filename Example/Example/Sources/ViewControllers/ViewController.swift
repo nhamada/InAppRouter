816866eee371e508366dc7e4214931cdfb15edb8
Naohiro Hamada <nao.pinguinus@icloud.com>
2018-02-06 23:13:09 +0900

    Create Example Project
    Create example project for InAppRouter

---
+++ Example/Example/Sources/ViewControllers/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Naohiro Hamada on 2018/02/06.
+//  Copyright © 2018年 Naohiro Hamada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
