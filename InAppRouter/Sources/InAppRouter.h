14d072c228d345e03bf090ddac8c7a71d75b8617
Naohiro Hamada <nao.pinguinus@icloud.com>
2018-02-06 23:06:51 +0900

    Create project
    Create framework project with Xcode.

---
+++ InAppRouter/Sources/InAppRouter.h
@@ -0,0 +1,19 @@
+//
+//  InAppRouter.h
+//  InAppRouter
+//
+//  Created by Naohiro Hamada on 2018/02/06.
+//  Copyright © 2018年 Naohiro Hamada. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InAppRouter.
+FOUNDATION_EXPORT double InAppRouterVersionNumber;
+
+//! Project version string for InAppRouter.
+FOUNDATION_EXPORT const unsigned char InAppRouterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InAppRouter/PublicHeader.h>
+
+
